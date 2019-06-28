546f2ac0453110a07656743c653fe0dcadec748e
superchenwb <chen1990wb@163.com>
2019-06-28 11:48:13 +0800

    修改参数大写为小写

--- android/src/main/java/com/zy/upgrade/RNUpgradeModule.java
+++ android/src/main/java/com/zy/upgrade/RNUpgradeModule.java
@@ -48,8 +48,8 @@ public class RNUpgradeModule extends ReactContextBaseJavaModule {
             .isWifiOnly(false)                                               //默认设置只在wifi下检查版本更新
             .isGet(true)                                                    //默认设置使用get请求检查版本
             .isAutoMode(false)                                              //默认设置非自动模式，可根据具体使用配置
-            .param("versionCode", versionCode)         //设置默认公共请求参数
-            .param("appKey", reactContext.getPackageName())
+            .param("versioncode", versionCode)         //设置默认公共请求参数
+            .param("appkey", reactContext.getPackageName())
             .setOnUpdateFailureListener(new OnUpdateFailureListener() {     //设置版本更新出错的监听
               @Override
               public void onFailure(UpdateError error) {
